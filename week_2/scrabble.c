c6e5c0a0ecf7ff106e6383ca238589af496c5977
Bitee-cd <calebduniya45@gmail.com>
2022-08-25 04:07:06 +0100

    scrabble

--- week_2/scrabble.c
+++ week_2/scrabble.c
@@ -1,37 +1,57 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
 #include <string.h>
 
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
 int main(void)
-
 {
-       char word[]="abc";
-        int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
 
-    //    (c>='A' && c<='Z') ? printf("yes"): printf("no");
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
 
-    // for (c = 'A'; c <= 'Z'; ++c)
-    //     printf("%d\n", (c - 'A') +1);
- 
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!");
+    }
+    else if (score2 > score1)
+    {
+        printf("Player 2 wins!");
+    }
+    else
+    {
+        printf("Tie!");
+    }
+}
+
+int compute_score(string word)
+{
     // variables declaration
-    int i,sum_of_word=0,number;
-    char upper='j';
-    i = ((upper)+(1%26));
-    printf("%c",(i));
+    int i, sum_of_word = 0, number;
+    char upper;
 
-    // //loop over the given string
-    // for(i=0;i<strlen(word);i++)
-    // {
-    //     //convert to uppercase
-    //     upper =tolower(word[i]);
-    //     // check and use for only alphabets
-    //     if(upper>='a' && upper<='z')
-    //     {
-    //         //find the position of the number in the alphabet
-    //        number = (upper - 'a') +1;
-    //         //use position as index to find the number in points array and add to sum
-    //        sum_of_word += POINTS[number-1];
-    //     }
-    // }
-    // printf("%d",sum_of_word);
+    //loop over the given string
+    for (i = 0; i < strlen(word); i++)
+    {
+        //convert to uppercase
+        upper = toupper(word[i]);
+        // check and use for only alphabets
+        if (upper >= 'A' && upper <= 'Z')
+        {
+            //find the position of the number in the alphabet
+            number = (upper - 'A') + 1;
+            //use position as index to find the number in points array and add to sum
+            sum_of_word += POINTS[number - 1];
+        }
+    }
+    return sum_of_word;
 }
